b1d72b282bcd949a579a0e16cfe0fa4fa1f86d82
SergGolub89 <k3210389gsi@gmail.com>
2019-12-09 13:09:43 +0200

    ipChanged

--- src/api/employees.js
+++ src/api/employees.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 export default function getEmployees (page) {
-  return axios.get(`http://localhost:3002/user/page/${(page - 1) * 5}`)
+  return axios.get(`http://192.168.0.167/user/page/${(page - 1) * 5}`)
 }
 
 export function createEmployee (user) {
-  return axios.post('http://localhost:3002/user/create', user)
+  return axios.post('http://192.168.0.167/user/create', user)
 }
 
 export function deleteEmployee (id) {
-  return axios.post(`http://localhost:3002/user/remove/${id}`)
+  return axios.post(`http://192.168.0.167/user/remove/${id}`)
 }
 
 export function editEmployee (id, user) {
-  return axios.post(`http://localhost:3002/user/edit/${id}`, user)
+  return axios.post(`http://192.168.0.167/user/edit/${id}`, user)
 }
