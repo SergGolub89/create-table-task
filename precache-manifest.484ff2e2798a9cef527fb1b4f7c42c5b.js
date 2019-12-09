cce46dfb2518f2a582837932f37f5752747c297a
SergGolub89 <k3210389gsi@gmail.com>
2019-12-09 12:48:00 +0200

    Updates

---
+++ precache-manifest.484ff2e2798a9cef527fb1b4f7c42c5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8a947da18d1ffed15a7937970c6def",
+    "url": "/create-table-task/index.html"
+  },
+  {
+    "revision": "58961d5a4bdba6f407c1",
+    "url": "/create-table-task/static/css/main.08c048bf.chunk.css"
+  },
+  {
+    "revision": "f535eff3b76296bd9c11",
+    "url": "/create-table-task/static/js/2.8cfed41a.chunk.js"
+  },
+  {
+    "revision": "58961d5a4bdba6f407c1",
+    "url": "/create-table-task/static/js/main.d2e50f4c.chunk.js"
+  },
+  {
+    "revision": "275c586584974578c818",
+    "url": "/create-table-task/static/js/runtime-main.4db36e65.js"
+  }
+]);
